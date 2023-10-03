2d8b1554fe2c3f7e826e8454d0edd9f5bed82083
chaojiezhang <chaojiezhang@microsoft.com>
2023-10-03 12:34:12 -0700

    files

--- vllm/model_executor/layers/attention.py
+++ vllm/model_executor/layers/attention.py
@@ -249,6 +249,7 @@ class PagedAttention(nn.Module):
             
             directory_path = './key_cache/'
             files = os.listdir(directory_path)
+            print(files)
             indices = [int(file.split('-')[1]) for file in files if file.startwith(f'gpu_{gpu_index}_tensor')]
             prev_layer = max(indices) if indices else 0
             cur_layer = prev_layer + 1
