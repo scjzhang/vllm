22914b2aee9a81b47ddaf03bddd791875c968b09
Inigo Goiri <elgoiri@gmail.com>
2023-10-11 17:00:21 -0700

    Update llm_engine_quality_compression.py

--- examples/llm_engine_quality_compression.py
+++ examples/llm_engine_quality_compression.py
@@ -1,10 +1,28 @@
 import argparse
 import os
+import re
 from datetime import datetime
 
 from vllm import EngineArgs, LLMEngine, SamplingParams
 
-file_name_pattern = r"qa_id_([0-9a-f]+)_words_([0-9]+)\.txt"
+data_folder_name = "/data/prompts_qa"
+
+def get_prompts():
+    prompts = []
+    prompt_ids = []
+    file_name_pattern = r"qa_id_([0-9a-f]+)_words_([0-9]+)\.txt"
+    sampling_param = SamplingParams(n=2, best_of=5, temperature=0.8, top_p=0.95, frequency_penalty=0.1)
+    for file_name in os.listdir(data_folder_name):
+        match = re.search(file_name_pattern, file_name)
+        if match:
+            prompt_id = match.group(1)
+            print(f"{prompt_id}: {file_name}")
+            with open(f"{data_folder_name}/{file_name}", "r", encoding="utf-8") as input_prompt_file:
+                prompt = input_prompt_file.read()
+                prompt_ids.append(prompt_id)
+                prompts.append((prompt, sampling_param))
+    return prompts, prompt_ids
+
 
 def main(args: argparse.Namespace):
     # Parse the CLI argument and initialize the engine.
@@ -15,16 +33,7 @@ def main(args: argparse.Namespace):
     engine = LLMEngine.from_engine_args(engine_args)
 
     # Test prompts from files.
-    test_prompts = []
-    folder_name = "/home/azureuser/prompts_qa"
-    for file_name in os.listdir(folder_name):
-        match = re.search(file_name_pattern, file_name)
-        if match:
-            qa_id = match.group(1)
-            print(f"{qa_id}: {file_name}")
-            with open(f"{folder_name}/{file_name}", "r", encoding="utf-8") as input_prompt_file:
-                prompt = input_prompt_file.read()
-                test_prompts.append((prompt, SamplingParams(n=2, best_of=5, temperature=0.8, top_p=0.95, frequency_penalty=0.1)))
+    test_prompts, test_prompt_ids = get_prompts()
 
     # Run the engine by calling `engine.step()` manually.
     request_id = 0
@@ -41,13 +50,15 @@ def main(args: argparse.Namespace):
         print(f"[{datetime.now()}] STEP {step_id}\n")
         for request_output in request_outputs:
             if request_output.finished:
-                #print(f"[{datetime.now()}] Request: {request_output.request_id}")
                 #print(f"[{datetime.now()}] Prompt: {request_output.prompt}")
-                print(f"[{datetime.now()}] Output: {request_output.outputs[0].text}")
+                req_id = int(request_output.request_id)
+                prompt_id = test_prompt_ids[req_id]
+                print(f"[{datetime.now()}] Output {req_id} {prompt_id}: {request_output.outputs[0].text}")
                 print()
 
                 # Output answer to measure quality with BARTSCore
-                with open(f"/home/azureuser/prompts_qa/output_{request_output.request_id}_{engine_args.compress_delta}.txt", "w") as output_file:
+                model_name = engine_args.model.replace('/', '_')
+                with open(f"{data_folder_name}/output_{prompt_id}_{engine_args.compress_delta}_{model_name}.txt", "w") as output_file:
                     output_file.write(request_output.outputs[0].text)
 
         if not (engine.has_unfinished_requests() or test_prompts):
